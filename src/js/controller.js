ff0529482ee97aa3ba1305e3af99de82967ff9e4
Codruta20 <codruta_bucuroiu@yahoo.com>
2022-05-31 19:32:41 +0300

    Implemented Search Results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
 const controlRecipes = async function () {
   try {
     const id = await window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -28,9 +22,19 @@ const controlRecipes = async function () {
   }
 };
 
-controlRecipes();
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchRecipe(query);
+    console.log(model.state.search.results);
+  } catch (err) {}
+};
+controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
